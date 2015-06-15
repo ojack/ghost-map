b19f8a171cdae45ed8cc44b30ac249fe870fea45
Olivia Jack <ojackson@gmail.com>
2015-06-15 15:02:17 -0400

    query overpass over https

--- utils/OSMquery.js
+++ utils/OSMquery.js
@@ -3,7 +3,7 @@ var request = require('browser-request');
 
 /* receives nodes within boundaries, and  as Lat-Ln coordinates as leaflet LatLn bounds*/
 exports.getWays = function (bounds, callback) {
-  var overpassUrl = 'http://overpass-api.de/api/interpreter';
+  var overpassUrl = 'https://overpass-api.de/api/interpreter';
   console.log(bounds);
   var query = overpassUrl+ "?data=[out:json];(way[\"highway\"]("+bounds.s+","+bounds.w+","+bounds.n+","+bounds.e+");node(w););out;";
     console.log(query);
